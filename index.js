7a4de451945d1fbc608b531fed09583b2ce77fd0
Abhinav Sharma <sharmaabhinav4279@gmail.com>
2022-06-23 22:49:06 +0530

    Add file based db

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const url = 'https://twitter.com/explore/tabs/trending';
 const getTrends = require('./lib/scraper')
+const fs = require('fs/promises')
 
 async function storeTrends() {
-  // console.log('hello')
-  await getTrends(url);
+  const cur_trends = await getTrends(url);
+  const all_trends_json = await fs.readFile('./db.json');
+  const all_trends = JSON.parse(all_trends_json);
+  all_trends.push(cur_trends);
+
+  await fs.writeFile('./db.json', JSON.stringify(all_trends, null, 2));
 }
 
 storeTrends();
